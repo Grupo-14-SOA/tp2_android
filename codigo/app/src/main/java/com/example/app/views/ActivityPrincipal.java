fa6313751452b38ae5970f4794add72ce584474d
ANTacchella <alejandronicolastacchella@gmail.com>
2021-11-10 22:35:38 -0300

    [ADD] Escaneo de QR y listado de url guardadas

--- codigo/app/src/main/java/com/example/app/views/ActivityPrincipal.java
+++ codigo/app/src/main/java/com/example/app/views/ActivityPrincipal.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.app.R;
 import com.example.app.presenters.Principal;
@@ -13,7 +15,8 @@ public class ActivityPrincipal extends AppCompatActivity {
 
     private Intent intentPrevio;
     private Principal presenter;
-    public IntentFilter filtro;
+    private IntentFilter filtro;
+    private Button buttonScanQR, buttonVerMenus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,22 @@ public class ActivityPrincipal extends AppCompatActivity {
 
         presenter = new Principal(this, refreshToken);
         configurarBroadcastReciever();
+
+        buttonScanQR = findViewById(R.id.buttonScanQR);
+        buttonVerMenus = findViewById(R.id.buttonVerMenus);
+
+        buttonScanQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ActivityPrincipal.this, EscanearQRActivity.class));
+            }
+        });
+        buttonVerMenus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ActivityPrincipal.this, MenusActivity.class));
+            }
+        });
     }
 
     private void configurarBroadcastReciever() {
---
+++ codigo/app/src/main/java/com/example/app/views/MenusActivity.java
@@ -0,0 +1,54 @@
+package com.example.app.views;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.app.R;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class MenusActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPref;
+    ListView lista;
+    ArrayList<String> listaMediciones;
+    ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menus);
+
+        sharedPref = getApplicationContext().getSharedPreferences(getString(R.string.archivo_menus), Context.MODE_PRIVATE);
+        listaMediciones = new ArrayList<>();
+        llenarLista();
+
+        lista = findViewById(R.id.listaMediciones);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaMediciones);
+
+        lista.setAdapter(arrayAdapter);
+    }
+
+    private synchronized void llenarLista(){
+        Map<String,?> keys = sharedPref.getAll();
+
+        for (Map.Entry<String,?> entry : keys.entrySet()){
+            listaMediciones.add(entry.getValue().toString());
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
